88bcb5563e73fa79aed570dfaa8bac203ce7678a
DMNihal65 <Nihaldm65@gmail.com>
2023-12-12 22:24:56 +0530

    main

---
+++ Week_01/Zeroda/main.js
@@ -0,0 +1,32 @@
+let a = 1;
+var b = 2;
+console.log(a)
+console.log(b)
+
+let name = "Nihal"
+let age = 18
+let ismarried = false;
+
+
+console.log(`first name ${name} and the age is ${age}`)
+
+//if else
+if(ismarried){
+    console.log(`first name ${name} is married`)
+}
+else{
+    console.log("not married")
+}
+
+let ans = null
+//loops
+for(let i=0;i<100;i++){
+    ans=ans+i
+}
+
+console.log(ans)
+
+//Arrays
+
+const person = ["John", "nihal","dm"]
+console.log(person[1])//nihal
