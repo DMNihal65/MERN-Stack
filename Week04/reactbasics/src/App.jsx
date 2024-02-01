fc206e3d463166424368372e74ab1607ba6ca385
DMNihal65 <Nihaldm65@gmail.com>
2024-02-01 22:13:09 +0530

    Reactbasic2

--- Week04/reactbasics/src/App.jsx
+++ Week04/reactbasics/src/App.jsx
@@ -1,19 +1,33 @@
+//importing the usestate hook to rerender the state variable
 import { useState } from 'react'
 import './App.css'
-//For the Dynamic Website
-function App() {
-  const [count, setCount] = useState(0)
+//For the Dynamic Website - State and Componets
 
-  return (
-    <div>
-        <p>Hi There</p>
-      
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-    </div>
-  )
+
+function App() {
+  //Defing the italial state variable
+   const [count,setCount] = useState(0);
+
+  // let state = {
+  //   count : 0
+  // }
+  // const [count, setCount] = useState(0)
+
+  function onClickHandler(){
+    // state.count = state.count + 1
+    // console.log(state.count)
+
+    //Set the state variable
+    setCount(count + 1);
+  }
+
+//  jsx - Javscript+xml
+
+
+//Componet
+function Custombutton(){
+
 }
 
 export default App
+ 
